cbd24aa64d1ded0b4be2917686799db15c1aeee1
Stanislava Piskyulieva <s.piskyulieva97@gmail.com>
2019-10-28 10:03:44 +0100

    extracting tlinks for drug events and all timex3

--- preprocessing/extract_treatment_events.py
+++ preprocessing/extract_treatment_events.py
@@ -1,21 +1,36 @@
 import os
 import xml.etree.ElementTree as ET
+import copy
 
 data_dir = "../data/raw_harvard_tlink"
 for file in os.listdir(data_dir):
     if not file.endswith('.xml'):
         continue
-
-    # print(file)
     f = open(os.path.join(data_dir, file), 'r')
     raw = f.read()
     raw_replaced = raw.replace("&", "&amp;")
-    # # parser = ET.XMLParser(encoding="utf-8")
+    new_root = ET.Element("root")
     try:
         root = ET.fromstring(raw_replaced)
     except ET.ParseError as e:
         print("CANT OPEN FILE: {}".format(file))
         print(e)
         pass
+
+    event_ids = set()
     for event in root.findall("./TAGS/EVENT/[@type='TREATMENT']"):
-        print(event)
+        event_ids.add(event.attrib['id'])
+        new_root.append(event)
+
+    timex3 = root.findall("./TAGS/TIMEX3")
+    for time in timex3:
+        new_root.append(time)
+
+    for eventID in event_ids:
+        tlinks = root.findall("./TAGS/TLINK")
+        for tlink in tlinks:
+            if tlink.attrib['fromID'] == eventID or tlink.attrib['toID'] == eventID:
+                new_root.append(tlink)
+
+    treatment_tree = ET.ElementTree(new_root)
+    treatment_tree.write(data_dir + "/treatment_events/" + file + "_modified.xml")
