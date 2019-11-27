a734f89c398de98a7b1865a7f91e326f6eec31aa
Stanislava Piskyulieva <s.piskyulieva97@gmail.com>
2019-11-27 13:42:22 +0000

    drug feature from clamp to help with rule based classification

--- preprocessing/extract_drugs.py
+++ preprocessing/extract_drugs.py
@@ -8,6 +8,7 @@ from collections import defaultdict
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction import DictVectorizer
 from numpy import sum
+from extract_labels import getLabels
 
 
 patterns = r'''(?x)
@@ -78,7 +79,7 @@ def getStartEndIndices(fileName):
     data_dir_features = "../data/raw_harvard_tlink/treatment_events"
     startIndices = set()
     endIndices = set()
-    f = open(os.path.join(data_dir_features, os.path.splitext(fileName)[0]), 'r')
+    f = open(os.path.join(data_dir_features, fileName.split('.')[0] + ".event.xml"), 'r')
     raw = f.read()
     root = ET.fromstring(raw)
     eventStart = root.findall("./EVENT")
@@ -113,16 +114,67 @@ def getTreatmentFeature(fileName):
         features.append(feature)
     return features
 
+def getDrugFeature(fileName):
+    features = []
+    data_dir_features = "../data/raw_harvard_tlink/section_ids/"
+    xmldoc = minidom.parse(data_dir_features + os.path.splitext(fileName)[0]+'.xmi')
+    drugsNER = xmldoc.getElementsByTagName("typesystem:ClampNameEntityUIMA")
+    drugStartIndices = []
+    for drug in drugsNER:
+        drugStartIndices.append(drug.getAttribute('begin'))
+    for i in range(len(words)):
+        startIndex = spans[i][0]
+        if treatmentsFeatureVector[i] is 'O-TREATMENT':
+            features.append('O')
+        elif str(startIndex) in drugStartIndices:
+            print("word is " + words[i])
+            features.append('DRUG')
+        else:
+            features.append('TREATMENT')
+
+    return features
+
 def getPOSFeature(POSVector):
     posTags = nltk.pos_tag(words)
     posDict = dict((x, y) for x, y in posTags)
     pos_vector = POSVector.transform(posDict)
     tf_idf_vector = tfidf_transformer.fit_transform(pos_vector)
-    print(tf_idf_vector.toarray())
+    # print(tf_idf_vector.toarray())
     return tf_idf_vector
+
+def ruleBasedClassifier():
+    correctLabels = getLabels(file)
+    predictedLabels = []
+    for i in range(len(treatmentsFeatureVector)):
+        if not treatmentsFeatureVector[i] == "B-TREATMENT":
+            continue
+        sectionId = sectionsFeatureVector[i]
+        if sectionId == "1" and drugFeatureVector[i] is 'DRUG':
+            predictedLabels.append("before")
+        elif sectionId == "1" and drugFeatureVector[i] is 'TREATMENT':
+            predictedLabels.append("during")
+        elif sectionId == "2":
+            predictedLabels.append("during")
+
+    correctPredictions = 0
+    print(file)
+    if len(correctLabels) < len(predictedLabels):
+        labelsLen = len(correctLabels)
+    else:
+        labelsLen = len(predictedLabels)
+    for i in range(labelsLen):
+        if correctLabels[i] == predictedLabels[i]:
+            correctPredictions+=1
+        else:
+            print("correct label: " + correctLabels[i] + " predicted label: " + predictedLabels[i])
+
+    return correctPredictions, len(correctLabels)
+
 
 data_dir = "../data/raw_harvard_tlink"
 POSVector = getPOSFeatureVector()
+positivePredictions = 0
+overallPredictions = 0
 for file in os.listdir(data_dir):
     if not file.endswith('.txt') or not os.path.isfile(os.path.join(data_dir, os.path.splitext(file)[0])):
         continue
@@ -135,5 +187,13 @@ for file in os.listdir(data_dir):
     treatmentsFeatureVector = getTreatmentFeature(file)
     sectionsFeatureVector = getSectionFeature(file)
     posFeatureVector = getPOSFeature(POSVector)
+    drugFeatureVector = getDrugFeature(file)
+    assert len(treatmentsFeatureVector) == len(sectionsFeatureVector) ==  len(drugFeatureVector)
+    currentPrediction, currentCount = ruleBasedClassifier()
+    positivePredictions += currentPrediction
+    overallPredictions += currentCount
     # assert len(treatmentsFeatureVector) == len(sectionsFeatureVector) ==  len(posFeatureVector)
     # break
+
+print("Accuracy is ")
+print((positivePredictions / overallPredictions) * 100)
--- preprocessing/extract_labels.py
+++ preprocessing/extract_labels.py
@@ -1,34 +1,30 @@
 import os
 import xml.etree.ElementTree as ET
 import copy
-import re
-def atoi(text):
-    return int(text) if text.isdigit() else text
-def natural_keys(text):
-    return [ atoi(c) for c in re.split('(\d+)',text) ]
 
 data_dir = "../data/raw_harvard_tlink/treatment_events"
-for file in os.listdir(data_dir):
-    if not file.endswith('.event.xml'):
-        continue
-    f = open(os.path.join(data_dir, file), 'r')
+def getLabels(file):
+    fileName = file.split('.')[0] + ".event.xml"
+    f = open(os.path.join(data_dir, fileName), 'r')
     rawEvent = f.read()
     try:
         event_root = ET.fromstring(rawEvent)
     except ET.ParseError as e:
-        print("CANT OPEN FILE: {}".format(file))
+        print("CANT OPEN FILE: {}".format(fileName))
         print(e)
         pass
 
-    f = open(os.path.join(data_dir, file.split('.')[0] + ".sectime.xml"), 'r')
+    f = open(os.path.join(data_dir, fileName.split('.')[0] + ".sectime.xml"), 'r')
     rawSecTime = f.read()
     secTime_root = ET.fromstring(rawSecTime)
 
-    f = open(os.path.join(data_dir, file.split('.')[0] + ".tlink.xml"), 'r')
+    f = open(os.path.join(data_dir, fileName.split('.')[0] + ".tlink.xml"), 'r')
     rawTLINK = f.read()
     tlink_root = ET.fromstring(rawTLINK)
 
     labels = []
+    admissionDate = 0
+    dischargeDate = 0
 
     for section in secTime_root.findall("./SECTIME"):
         if section.attrib['type'].lower() == "admission":
@@ -39,24 +35,23 @@ for file in os.listdir(data_dir):
     sorted_event_root = sorted(event_root, key=lambda child: (child.tag,int(child.get('start'))))
 
     for event in sorted_event_root:
-        print(event.attrib['start'])
-        print(file)
         tlink = tlink_root.find('./TLINK[@fromID="{value}"]'.format(value=event.attrib['id']))
         if tlink is None:
             labels.append("n/a")
             continue
         tlinkType =  tlink.attrib['type'].lower()
-        tlinkValue = tlink.attrib['toText']
+        tlinkValue = tlink.attrib['toText'].lower()
         if tlinkType == "overlap" or tlinkType == "before_overlap":
             labels.append("during")
-        elif tlinkValue == admissionDate:
+        elif tlinkValue == admissionDate or tlinkValue == "admission":
             if tlinkType == "before":
                 labels.append("before")
             elif tlinkType == "after":
                 labels.append("during")
-        elif tlinkValue == dischargeDate:
+        elif tlinkValue == dischargeDate or tlinkValue == "discharge":
             if tlinkType == "before":
                 labels.append("during")
             elif tlinkType == "after":
                 labels.append("after")
-    print(labels)
+
+    return labels
